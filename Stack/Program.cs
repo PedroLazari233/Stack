cd3b450f621ed1918bc9491f552829e7829c2898
Pedro Lazari <67986647+PedroLazari233@users.noreply.github.com>
2023-05-27 10:13:41 -0300

    Full project

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var stack = new Stack.Stack();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
     }
 }
---
+++ Stack/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private int _top;
+        private List<object> _elements;
+
+        public Stack()
+        {
+            _elements = new List<object>();
+            _top = -1;
+        }
+
+        private bool IsEmpty()
+        {
+            return _top == -1;
+        }
+
+        public void Push(object obj)
+        {
+            if(obj == null)
+            {
+                Console.WriteLine("Error: Can't pass empty objects!");
+                return;
+            }
+
+            _top++;
+            _elements.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("Error: Tha stack is empty!");
+                return -1;
+            }
+
+            object obj = _elements[_top];
+            _top--;
+            return obj;
+        }
+
+        public void Clear()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("Stack is already empty!");
+                return;
+            }
+
+            _top = -1;
+            _elements.Clear();
+        }
+    }
+}
